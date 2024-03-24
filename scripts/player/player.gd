931a2c4edd2d754a734493f6060f43671b675d69
Eusébio Alves <eusebio.a95@hotmail.com>
2024-03-24 22:34:17 +0000

    Added sprint stamina mechanic to player
    Sprint is now limited to ~4 seconds, once depleted ~4 seconds of 'cooldown' before able to sprint again.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,22 +4,36 @@ extends CharacterBody2D
 @export var MAX_RUN_SPEED : float = 75.0
 @export var ACCEL_RATE : float = 5
 @export var DASH_STRENGTH : float = 200.0
+@export var MAX_STAMINA : float = 4
 
 var current_speed : float
+var current_stamina : float
 var input_direction : Vector2
+var fatigued : bool = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	current_speed = MAX_WALK_SPEED
+	current_stamina = MAX_STAMINA
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	current_speed = MAX_RUN_SPEED if (Input.is_action_pressed("sprint")) else MAX_WALK_SPEED
 	
-	print(velocity)
+	current_speed = MAX_WALK_SPEED
+	if (Input.is_action_pressed("sprint") && !fatigued):
+		current_speed = MAX_RUN_SPEED
 	
-func _physics_process(delta):
+	stamina_manager(delta)
+	
+func _physics_process(_delta):
 	velocity = velocity.move_toward(input_direction * current_speed, ACCEL_RATE)
 	move_and_slide()
 	
+func stamina_manager(delta):
+	if current_speed == MAX_RUN_SPEED:
+		current_stamina -= delta
+		if current_stamina <= 0: fatigued = true
+	else:
+		if current_stamina < MAX_STAMINA:
+			current_stamina += delta
+		else:
+			fatigued = false
