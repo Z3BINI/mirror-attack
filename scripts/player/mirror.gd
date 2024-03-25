1348665be06b4e804eae5111117dcb38c7500ec8
Eusébio Alves <eusebio.a95@hotmail.com>
2024-03-25 10:22:17 +0000

    Added basic script to mirror
    Mirror rotates around player based on mouse cursor.

---
+++ scripts/player/mirror.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+var smooth_mouse_pos : Vector2
+
+@onready var player_hand : Node2D = get_parent()
+
+func _physics_process(delta):
+	smooth_mouse_pos = lerp(smooth_mouse_pos, get_global_mouse_position(), 0.3)
+	player_hand.look_at(smooth_mouse_pos)
