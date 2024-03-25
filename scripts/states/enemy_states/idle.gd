8f19c921cf48abd2e35054b768a258f242e925dc
Eusébio Alves <eusebio.a95@hotmail.com>
2024-03-25 10:59:23 +0000

    Created basic idle and roam state

---
+++ scripts/states/enemy_states/idle.gd
@@ -0,0 +1,24 @@
+extends State
+class_name  Idle
+
+@export var MIN_IDLE_TIME : float = 5
+@export var MAX_IDLE_TIME : float = 10
+
+var idle_time : float
+
+func randomize_idle_time():
+	idle_time = randf_range(MIN_IDLE_TIME, MAX_IDLE_TIME)
+
+func enter_state():
+	randomize_idle_time()
+
+func update(delta):
+	if idle_time > 0:
+		idle_time -= delta
+	else:
+		state_changed.emit(self, 'roam')
+
+func physics_update(_delta):
+	if self_body:
+		self_body.velocity = Vector2.ZERO
+
