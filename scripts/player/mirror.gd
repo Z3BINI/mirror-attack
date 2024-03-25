e794bc5f7a3667f0e33815a7925ec895a81c3456
Eusébio Alves <eusebio.a95@hotmail.com>
2024-03-25 11:37:38 +0000

    Unused delta var

--- scripts/player/mirror.gd
+++ scripts/player/mirror.gd
@@ -4,6 +4,6 @@ var smooth_mouse_pos : Vector2
 
 @onready var player_hand : Node2D = get_parent()
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	smooth_mouse_pos = lerp(smooth_mouse_pos, get_global_mouse_position(), 0.3)
 	player_hand.look_at(smooth_mouse_pos)
