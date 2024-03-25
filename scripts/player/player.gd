19cb07f55620c4fca612a5e2e17982c58df489a2
Eusébio Alves <eusebio.a95@hotmail.com>
2024-03-25 09:38:48 +0000

    Added dash mechanic & cleaner stamina management
    Promoting readability.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var MAX_RUN_SPEED : float = 75.0
 @export var ACCEL_RATE : float = 5
 @export var DASH_STRENGTH : float = 200.0
+@export var DASH_COST : int = 2
 @export var MAX_STAMINA : float = 4
 
 var current_speed : float
@@ -22,18 +23,33 @@ func _process(delta):
 	if (Input.is_action_pressed("sprint") && !fatigued):
 		current_speed = MAX_RUN_SPEED
 	
+	if (Input.is_action_just_pressed("dash") && !fatigued):
+		dash()
+		set_exhausted()
+	
 	stamina_manager(delta)
 	
 func _physics_process(_delta):
 	velocity = velocity.move_toward(input_direction * current_speed, ACCEL_RATE)
 	move_and_slide()
 	
+func dash():
+	velocity = input_direction * DASH_STRENGTH
+
 func stamina_manager(delta):
 	if current_speed == MAX_RUN_SPEED:
 		current_stamina -= delta
-		if current_stamina <= 0: fatigued = true
+		if current_stamina <= 0: set_exhausted()
 	else:
 		if current_stamina < MAX_STAMINA:
 			current_stamina += delta
 		else:
-			fatigued = false
+			set_replenished()
+
+func set_exhausted():
+	current_stamina = 0
+	fatigued = true
+	
+func set_replenished():
+	current_stamina = MAX_STAMINA
+	fatigued = false
