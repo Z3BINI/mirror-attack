2ebafad6d6dd56f33c49aa199e3a44c60652f965
Eusébio Alves <eusebio.a95@hotmail.com>
2024-03-26 15:15:27 +0000

    When player near door listen for input

--- scripts/components/door_check.gd
+++ scripts/components/door_check.gd
@@ -3,10 +3,12 @@ extends Area2D
 func _on_body_entered(_body):
 	if get_parent().room_cleared:
 		$PassLabel.visible = true
+		get_parent().player_on_door = true
 	else:
 		$BlockLabel.visible = true
 	
 func _on_body_exited(_body):
-		$PassLabel.visible = false
-		$BlockLabel.visible = false
+	get_parent().player_on_door = false
+	$PassLabel.visible = false
+	$BlockLabel.visible = false
 	
--- scripts/levels/levels.gd
+++ scripts/levels/levels.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var NEXT_LEVEL : PackedScene
 
 var room_cleared : bool = false
+var player_on_door : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,4 +13,8 @@ func _ready():
 func _process(delta):
 	if ($Enemies.get_children().size() <= 0):
 		room_cleared = true
+	if Input.is_action_pressed("interact") and player_on_door: 
+		load_next_level()
 	
+func load_next_level():
+	get_tree().change_scene_to_packed(NEXT_LEVEL)
