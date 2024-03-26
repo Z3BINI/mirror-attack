ec89b160f031b3c5b3088466d438d1d869e07b45
Eusébio Alves <eusebio.a95@hotmail.com>
2024-03-26 15:15:44 +0000

    Fixed die spam on update

--- scripts/components/hurt_box.gd
+++ scripts/components/hurt_box.gd
@@ -2,10 +2,6 @@ extends Area2D
 
 var MAX_HP : float = 10
 
-func _process(delta):
-	if MAX_HP <= 0:
-		die()
-	
 func _on_area_entered(area):
 	if (get_parent().is_in_group("enemy") and !area.is_hot): return
 	take_dmg(area.DMG_AMOUNT, area.global_position)
@@ -15,10 +11,11 @@ func take_dmg(amount, dir):
 	get_parent().took_dmg = true
 	get_parent().knock_back(dir)
 	MAX_HP -= amount
-	print("dmged")
+	if MAX_HP <= 0:
+		die()
 	
 func die():
-	get_parent().dead = true
-	
+	if get_parent().name == "Player": get_parent().die()
+	else: get_parent().dead = true
 
 
