436095e5fa3bf2e3479fd4eaf732b0d7a9ced736
Eusébio Alves <eusebio.a95@hotmail.com>
2024-03-27 16:40:22 +0000

    store current hp on level load

--- scripts/levels/levels.gd
+++ scripts/levels/levels.gd
@@ -7,7 +7,7 @@ var player_on_door : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	PlayerStats.player_load_hp = PlayerStats.hp
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
