717c1d7d1c8b18dfb70b47056e2a199777d6c461
Eusébio Alves <eusebio.a95@hotmail.com>
2024-03-27 20:43:58 +0000

    audio manager for sfx autoload

--- scripts/components/audio_manager.gd
+++ scripts/components/audio_manager.gd
@@ -12,5 +12,14 @@ func _ready():
 func _process(delta):
 	pass
 
-func playsfx(node, sfx):
-	pass
+func play_sfx(sound : AudioStream, parent_node : Node, volume : float):
+	if sound != null or parent_node != null:
+		var stream = AudioStreamPlayer2D.new()
+		
+		stream.stream = sound
+		stream.volume_db = volume
+		
+		stream.connect('finished', stream.queue_free)
+		
+		parent_node.add_child(stream)
+		stream.play()
