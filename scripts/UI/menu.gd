f5282a586e2d4e82e5e7fd6ffca9982b73feb851
Eusébio Alves <eusebio.a95@hotmail.com>
2024-03-27 16:38:42 +0000

    Retry level, restartr game and quit

---
+++ scripts/UI/menu.gd
@@ -0,0 +1,42 @@
+extends Control
+
+@onready var pause_menu = $Pause
+@onready var dead_menu = $Dead
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Pause/MusicToggle.button_pressed = AudioManager.main_theme.playing
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func toggle_pause():
+	pause_menu.visible = !pause_menu.visible
+
+func show_dead():
+	dead_menu.visible = true
+
+func _on_return_pressed():
+	pause_menu.visible = false
+	get_tree().paused = false
+
+
+func _on_music_toggle_toggled(toggled_on):
+	if !toggled_on: AudioManager.main_theme.stop()
+	else: AudioManager.main_theme.play()
+
+
+func _on_retry_pressed():
+	PlayerStats.hp = PlayerStats.player_load_hp
+	PlayerStats.dead = false
+	get_tree().reload_current_scene()
+	
+func restart_game():
+	PlayerStats.hp = PlayerStats.MAX_HP
+	PlayerStats.dead = false
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
